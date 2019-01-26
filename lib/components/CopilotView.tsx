4bfd85b501efcb91fcdee844911cc2b49556404f
choif <koreanwglasses@gmail.com>
2019-01-25 20:57:29 -0500

    Removed hidden prop

--- lib/components/CopilotView.tsx
+++ lib/components/CopilotView.tsx
@@ -5,76 +5,75 @@ import { EtchComponent } from './EtchComponent';
 import { Footer } from './Footer';
 
 export interface CopilotViewProps extends JSX.Props {
-    hidden?: boolean;
     currentView?: string; 
-
+    
     projectSetupProps?: ProjectSetupProps;
     startupPageProps?: StartupPageProps;
 }
 
 /**
-* This is the rool component for the sidebar. Eventually other things
-* such as resizing, title, tabs, settings, etc. will be implemented.
-*/
+ * This is the rool component for the sidebar. Eventually other things
+ * such as resizing, title, tabs, settings, etc. will be implemented.
+ */
 export class CopilotView extends EtchComponent {
     public props: CopilotViewProps;
-
+    
     private currentView: string; 
-
+    
     constructor(props: CopilotViewProps) {
         super(props);
-
+        
         // Loading personal props
         this.setView(props.currentView || "StartupPage", false);
-
+        
         // Initialization
         etch.initialize(this);
     }
-
+    
     getTitle(): string {
         return "Copilot";
     }
-
+    
     getDefaultLocation(): string {
         return "right";
     }
-
+    
     render(): JSX.Element {
         return (
             <div class="copilot">
-                <div class={this.currentView == "projectsetup" ? "" : "hidden"}>
-                    <ProjectSetup {...this.props.projectSetupProps} parent={this} ref="projectSetup"/>
-                </div>
-                <div class={this.currentView == "startuppage" ? "" : "hidden"}>
-                    <StartupPage {...this.props.startupPageProps} parent={this} ref="startupPage"/>
-                </div>
-                <Footer />
+            <div class={this.currentView == "projectsetup" ? "" : "hidden"}>
+            <ProjectSetup {...this.props.projectSetupProps} parent={this} ref="projectSetup"/>
             </div>
-        );
-    }
-
-    serialize(): CopilotViewProps {
-        return {
-            currentView: this.currentView,
-
-            projectSetupProps: this.refs.projectSetup.serialize(),
-            startupPageProps: this.refs.startupPage.serialize()
+            <div class={this.currentView == "startuppage" ? "" : "hidden"}>
+            <StartupPage {...this.props.startupPageProps} parent={this} ref="startupPage"/>
+            </div>
+            <Footer />
+            </div>
+            );
+        }
+        
+        serialize(): CopilotViewProps {
+            return {
+                currentView: this.currentView,
+                
+                projectSetupProps: this.refs.projectSetup.serialize(),
+                startupPageProps: this.refs.startupPage.serialize()
+            }
+        }
+        
+        /**
+        * Sets the view to the specified view.
+        * @param viewName Can be one of:
+        * *ProjectSetup
+        * *StartupPage
+        * @param updateNow Controls whether the component should update immediately to
+        * reflect the changed view. True by default.
+        */
+        setView(viewName: string, updateNow?: boolean): void {
+            this.currentView = viewName.toLowerCase(); 
+            
+            if(updateNow === undefined || updateNow) {
+                etch.update(this);
+            }
         }
     }
-
-    /**
-     * Sets the view to the specified view.
-     * @param viewName Can be one of:
-     * *ProjectSetup
-     * *StartupPage
-     * @param updateNow Controls whether the component should update immediately to
-     * reflect the changed view. True by default.
-     */
-    setView(viewName: string, updateNow?: boolean): void {
-        this.currentView = viewName.toLowerCase(); 
-
-        if(updateNow === undefined || updateNow) {
-            etch.update(this);
-        }
-    }
-}
