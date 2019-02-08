371404c40f2bd79a1e4d9d734ac9063d8adb5699
eagraf <graf.ethan@gmail.com>
2019-02-07 19:09:59 -0500

    Basic browsing list for projects.
    - Added axios dependency for HTTP requests
    - Make GET request to Github API to get all repos in the
      spaceport-curriculums repository.
    - Display projects in a list.

--- lib/components/BrowseProjects.tsx
+++ lib/components/BrowseProjects.tsx
@@ -2,11 +2,24 @@ import * as etch from 'etch';
 import { CopilotView } from './CopilotView';
 import { Copilot } from '../models/Copilot'
 import { EtchComponent } from './EtchComponent';
+import axios from 'axios';
 
-export interface BrowseProjectsProps extends JSX.Props {}
+export interface BrowseProjectsProps extends JSX.Props {
+    projects: ProjectInfo[];
+}
 
 /**
-* An test view that sets up projects
+* Basic data about a project.
+*/
+export interface ProjectInfo {
+    title: string;
+    description: string;
+    topic: string;
+    url: string;
+}
+
+/**
+* View for borwsing available projects.
 */
 export class BrowseProjects extends EtchComponent {
     public props: BrowseProjectsProps;
@@ -15,18 +28,66 @@ export class BrowseProjects extends EtchComponent {
 
     public constructor(props: BrowseProjectsProps) {
         super(props);
+        this.props.projects = [];
         etch.initialize(this);
+        axios.get("https://api.github.com/orgs/spaceport-curriculums/repos", {
+            headers: {
+                Accept: "application/vnd.github.mercy-preview+json"
+            }
+        })
+        .then((response: any) => {
+            let projects = [];
+            const data = response.data;
+            for (let i = 0; i < data.length; i++) {
+                let project = {
+                    title: data[i].name,
+                    description: data[i].description,
+                    topic: data[i].topics[0],
+                    url: data[i].clone_url
+                }
+                projects.push(project);
+            }
+            this.props.projects = projects;
+            etch.update(this);
+        })
+        .catch((error: any) => {
+            console.log(error);
+        });
     }
 
     public render(): JSX.Element {
+        const projectList = this.props.projects.map((project) =>
+            <div class="project-tile">
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
+                <p>{project.topic}</p>
+                <input type="button" class="btn" on={{click: () => this.startProject(project.url)}} value="Start Project" />
+            </div>
+        );
         return (
-            <div>
+            <div class="box">
                 <h2>Browse Projects</h2>
+                <div class="project-list section scroll">{projectList}</div>
             </div>
         );
     }
 
     serialize(): BrowseProjectsProps {
-        return {};
+        return {
+            projects: this.props.projects
+        };
+    }
+
+    startProject(source: string): void {
+        const model = Copilot.getInstance();
+
+        const promise = model.setupProject(source);
+        promise.then((value) => {
+            atom.notifications.addSuccess("Project successfully set up!");
+        });
+        promise.catch((reason) => {
+            atom.notifications.addError("Uh oh! Something went wrong while trying to set up the project.");
+            atom.notifications.addError(reason.message);
+        })
     }
 }
